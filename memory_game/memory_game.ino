b850a549c75d58f71fa3addae6b89137f8c72665
carrotfarm02 <wyattalanbach@gmail.com>
2021-12-12 17:21:05 -0500

    Added Arduino files to our project.

---
+++ memory_game/memory_game.ino
@@ -0,0 +1,196 @@
+const int button1 = 2;
+const int button2 = 3;
+const int button3 = 4;
+const int button4 = 5;
+const int led1 = 7; //yellow LED
+const int led2 = 8; //green LED
+const int led3 = 9; //red LED
+const int led4 = 10; //blue LED
+const int buzzer = 12;
+const int tones[] = {1900, 1600, 1300, 1000, 3200};
+int buttonState [] = {0,0,0,0};
+
+int lastButtonstate[] = {0,0,0,0};
+int buttonCounter[] = {0,0,0,0};
+int gameOn = 0;
+int wait = 0;
+int currentLevel = 1;
+int dlay = 500;
+int ledTime = 500;
+int n_levels = 6; // current number of levels; 
+int n_levels_orig = n_levels; // fall back to orig on fail
+int pinAndTone = 0;
+int correct = 0;
+int speedFactor = 5;
+int ledDelay = 200;
+
+void playTone (int tone, int duration){
+  for (long i = 0; i < duration * 1000L; i += tone*2)
+  {
+    digitalWrite(buzzer, HIGH);
+    delayMicroseconds(tone);
+    digitalWrite(buzzer, LOW);
+    delayMicroseconds(tone);
+  }
+}
+
+void winnerTune(){
+  playTone(700, 100);
+  playTone(500, 100);
+  playTone(200, 100);
+  playTone(100, 100);
+}
+
+void looserTune(){
+  playTone(100, 200);
+  playTone(200, 200);
+  playTone(500, 200);
+  playTone(700, 200);
+
+  //buzz
+  delay(100);
+  playTone(8000, 100);
+  delay(100);
+  playTone(8000, 100);
+}
+
+void looserTuneOrig(){
+    int i;
+    for (i=0; i < 4; i = i + 1){
+     digitalWrite(i+7, HIGH);
+    }
+    playTone(tones[4], ledTime);
+    for (i=0; i < 4; i = i + 1){
+     digitalWrite(i+7, LOW);
+    }
+    delay(200);
+    for (i=0; i < 4; i = i + 1){
+     digitalWrite(i+7, HIGH);
+    }
+    playTone(tones[4], ledTime);
+    for (i=0; i < 4; i = i + 1){
+     digitalWrite(i+7, LOW);
+    }   
+}
+void setup() {
+  randomSeed(analogRead(0));
+
+  pinMode(button1, INPUT);
+  pinMode(button2, INPUT);
+  pinMode(button3, INPUT);
+  pinMode(button4, INPUT);
+
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+}
+void loop() {
+  int n_array[n_levels];
+  int u_array[n_levels];
+  int i;
+  if (gameOn == 0 ){
+    for (i=0; i < n_levels; i=i+1){
+      n_array[i]=0;
+      u_array[i]=0;
+      n_array[i]=random(0,4);
+    }
+    gameOn=1;
+  }
+  if (wait == 0) {
+    delay (200);
+    i = 0;
+    for (i = 0; i < currentLevel; i=i+1){
+      ledDelay = ledTime/(1+(speedFactor/n_levels) * (currentLevel - 1));
+      pinAndTone = n_array[i];
+      digitalWrite(pinAndTone +7, HIGH);
+      playTone(tones[pinAndTone ], ledDelay);
+      digitalWrite(pinAndTone +7, LOW);
+      delay(100/speedFactor);
+    }
+    wait = 1;
+  }
+  i = 0;
+  int buttonChange = 0;
+  int j = 0;
+  while (j < currentLevel){
+    while (buttonChange == 0){
+      for (i = 0; i < 4; i=i+1){
+        buttonState[i] = digitalRead(i+2);
+        buttonChange+= buttonState[i];
+      }
+    }
+    for (i = 0; i < 4; i=i+1){
+      if (buttonState[i] == HIGH) {
+        digitalWrite(i+7, HIGH);
+        playTone(tones[i], ledTime);
+        digitalWrite(i+7, LOW);
+        wait = 0;
+        u_array[j]=i;
+        buttonState[i] = LOW;
+        buttonChange=0;
+      }
+    }
+    if (u_array[j] == n_array[j]){
+      j++;
+      correct = 1;
+    }
+    else{
+      correct = 0;
+      i = 4;
+      j = currentLevel;
+      wait = 0;
+    }
+  }
+  if (correct == 0){
+    delay(300);
+    i = 0;
+    gameOn=0;
+    currentLevel=1;
+    n_levels - n_levels_orig;
+    
+    for (i=0; i < 4; i = i + 1){
+      digitalWrite(i+7, HIGH);
+    }
+    looserTune();
+    for (i=0; i < 4; i = i + 1){
+      digitalWrite(i+7, LOW);
+    }
+    
+    delay(500);
+    gameOn = 0;
+  }
+  if (correct == 1){
+    currentLevel++;
+    delay (100);
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, HIGH);
+    winnerTune();
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
+  }
+  if (currentLevel == n_levels){
+    delay(500);
+    int notes[] = {2, 2, 2,2, 0, 1, 2, 1, 2};
+    int note = 0;
+    int tempo[] {200, 200, 200, 400, 400, 400, 200, 200, 600};
+    int brakes[] = {100, 100, 100, 200, 200, 200, 300, 100, 200};
+    for (i=0; i<9; i=i+1){
+      note = notes[i];
+      digitalWrite(note+7, HIGH);
+      playTone(tones[note], tempo[i]);
+      digitalWrite(note+7, LOW);
+      delay(brakes[i]);
+    }
+    gameOn = 0;
+    currentLevel = 1;
+    n_levels = n_levels + 2;
+    speedFactor += 1;
+    
+  }
+}
---
+++ mood_lamp_seziure/mood_lamp_seziure.ino
@@ -0,0 +1,111 @@
+/* Creation Crate Mood Lamp
+    This lamp smoothly cycless through a colour spectrum
+    It only turns on when its sourroundings are dark
+    Color equations : we need to know dis ltr!
+      Red = sin(x)
+      Green = sin(x + PI/3)
+      Blue = sin(x + 2*PI/3)
+      These equations are how the program will calculate the brightness of the LEDs.
+
+      Question: what is X? Time? Sensor??
+*/
+
+/* Step 1: Input User Defined Variables
+    Think of vars like containers---the store values!
+    You can replace this with any whole number
+*/
+int pulseSpeed = 5; // orig val 5
+
+// These are the pins we are using with the UNO R3 (Arduino compatible)
+// Yutes can see these nums on the board itself
+
+int ldrPin = 0; //LDR in Analog Input 0 to read the surrounding light
+int redLed = 11; // red LED in Digital pin 11
+int greenLed = 10; // green LED in Digital pin 10
+int blueLed = 9; // blue LED in Digital pin 9
+
+int ambientLight; // This variable stores the value of the light in the room
+int power = 150; // This variable controls the brightness of the lamp (2-255)
+
+float RGB[3]; // This is an 'array'. It can hold 3 values: RGB[0], RGB[1], RGB[2]. We'll use this to store the values of Red, Blue and Green LEDs.
+
+/* We will be using the value of 180/PI a lot in the main loop so to save time we will calculate
+    it once here in the setup and store it in CommonMathVariable. Note: it is PI(i), not P1(one)
+*/
+float CommonMathVariable = 180 / PI;
+
+/* Step 2: Create Setup Loop
+    This 'loop' is not really a loop. It runs once in the beginning to create the default
+    values for our LEDs.
+*/
+
+void setup() {
+  // This tells the UNO R3 to send data out to the LEDs
+  pinMode (redLed, OUTPUT);
+  pinMode (greenLed, OUTPUT);
+  pinMode (blueLed, OUTPUT);
+
+  //This sets all the output (LEDs) to low (as in off), so that they do not turn on during startup
+  digitalWrite(redLed, LOW);
+  digitalWrite(greenLed, LOW);
+  digitalWrite(blueLed, LOW);
+} // Opening brackets must be accompanied by closing brackets
+
+/* Step 3: Create Main Loop
+    The previous sections are where we set up the variables. This section is where we put them to work!!
+    This part of the program is a real 'loop'. It repeats itself over and over again,
+    making small changes in the brightness of the LEDs each time - this creates a smooth transition
+    in the coloooouuurrr.
+*/
+
+void loop () {
+  for (float x = 0; x < PI; x = x + 0.25) {
+    RGB[0] = power * abs(sin(x * (CommonMathVariable)));
+    RGB[1] = (power/2) * abs(sin( (x + PI / 3) * (CommonMathVariable)));
+    RGB[2] = power * abs(sin( (x + (2 * PI) / 3) * (CommonMathVariable)));
+
+    // This reads the light in the room and stores it as a number
+    ambientLight = analogRead(ldrPin);
+
+    // This 'if statement' will make the lamp turn on only if it is dark.
+    // The darker it is the higher the number
+    if (ambientLight > 800) {
+      // These 'analog write' statements will send the values calculated above to the LEDs
+      analogWrite(redLed, RGB[0]);
+      analogWrite(greenLed, RGB[1]);
+      analogWrite(blueLed, RGB[2]);
+    } // don't forget to close the 'if statement' with bracket, you punk!
+    else  // This 'else statement' will only activate if the 'if statement' above does not (i.e. it is too bright
+      // in the room). The LEDs will turn off. (or else!!!)
+    {
+      analogWrite(redLed, LOW);
+      analogWrite(greenLed, LOW);
+      analogWrite(blueLed, LOW);
+    }
+    // This loop calculates the delay for each color depending on its current brightness. Brighter LEDs will change color slower
+    // and vice versa.
+    for (int i = 0; i < 3; i = i + 1) {
+      if (RGB[i] < 1) {
+        delay (20 * pulseSpeed); // delay by 20 pulses by default
+      }
+      // 'else if' means only one of these conditions can activate at a time
+      else if (RGB[i] < 5) {
+        delay (10 * pulseSpeed);
+      }
+
+      else if (RGB[i] < 10) {
+        delay (2 * pulseSpeed);
+        
+      }     
+      else if (RGB[i] < 100) {
+        delay (1 * pulseSpeed); 
+      }
+      // This blank 'else statement' is a fail-safe mechanism. It instructs the program to do nuffin
+      // if the conditions above don't activate. This prevents the programme from generating errors when calculating delays.
+      else {}
+    }
+    // This delay gives the light-dependent resistoid time to settle and give accurate readings.
+   
+    delay(1);
+  }
+}// Remember to close yer brackets!!
